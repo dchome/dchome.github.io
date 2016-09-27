1e075c8b7762ff17da735f979517edb3da5f41fc
Daniel Homer <daniel.homer6@gmail.com>
2016-09-27 15:31:18 -0400

    Add jquery scrolling to nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,36 @@
-//
+function scrollToSection(section) {
+  console.log('it worked');
+  $('html, body').animate({
+    scrollTop: section.offset().top
+  }, 600);
+} 
+
+$(document).ready(function(){
+  var windowHeight = $(window).height();
+
+  $(window).resize(function(){
+    windowHeight = $(window).height();
+    console.log('window height is ' + windowHeight);
+  });
+
+  $("#home-control").on('click', function(event) {
+    scrollToSection($('#home-section'));
+  });
+
+  $("#about-control").on('click', function(event){
+    scrollToSection($('#about-section'));
+  });
+
+  $("#portfolio-control").on('click', function(event){
+    scrollToSection($('#portfolio-section'));
+  });
+
+  $("#resume-control").on('click', function(event){
+    scrollToSection($('#resume-section'));
+  })
+
+  $("#contact-control").on('click', function(event){
+    scrollToSection($('#contact-section'));
+  })
+
+});
