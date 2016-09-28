6f1fbd3b2eadc305759a5fedd6107ac56d6f9aee
Daniel Homer <daniel.homer6@gmail.com>
2016-09-28 14:11:54 -0400

    add event handlers for about radio buttons

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,12 @@ function scrollToSection(section) {
   $('html, body').animate({
     scrollTop: section.offset().top
   }, 600);
-} 
+}
+
+function fadeAboutContent() {
+  $('.portrait').fadeOut(600);
+  $('.portrait-p').fadeOut(600);
+}
 
 $(document).ready(function(){
   var windowHeight = $(window).height();
@@ -32,5 +37,33 @@ $(document).ready(function(){
   $("#contact-control").on('click', function(event){
     scrollToSection($('#contact-section'));
   })
+
+  $('#little-control').on('click', function(event){
+    fadeAboutContent();
+
+    $('#little-dan').fadeIn(600);
+    $('#little-dan-p').fadeIn(600);
+  });
+
+  $('#college-control').on('click', function(event){
+    fadeAboutContent();
+
+    $('#college-dan').fadeIn(600);
+    $('#college-dan-p').fadeIn(600);
+  });
+
+  $('#lawyer-control').on('click', function(event){
+    fadeAboutContent();
+
+    $('#lawyer-dan').fadeIn(600);
+    $('#lawyer-dan-p').fadeIn(600);
+  });
+
+  $('#computer-control').on('click', function(event){
+    fadeAboutContent();
+
+    $('#computer-dan').fadeIn(600);
+    $('#computer-dan-p').fadeIn(600);
+  });
 
 });
